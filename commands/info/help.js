02d829d9ec7823c30efe15c959513cec1e029ac2
DaBoltGod <82177322+daboltgod@users.noreply.github.com>
2024-04-10 21:56:07 -0700

    Create help.js

---
+++ commands/info/help.js
@@ -0,0 +1,9 @@
+client.commands({
+  name: "help",
+  code: `
+  $title[What's this]
+  $description[You need help? Heres some commands I have \n
+  help, ping]
+  $footer[Made by FuelEx]
+  $color[Orange]
+  `
