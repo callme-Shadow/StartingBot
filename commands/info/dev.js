2451a288033ffa5d11025e5e3f6a279e73022bf0
Shadow <shadowstriker169@gmail.com>
2024-04-14 16:05:50 +0530

    Create dev.js

---
+++ commands/info/dev.js
@@ -0,0 +1,8 @@
+client.commands({
+  name: "dev",
+  code: `
+  $title[Contact My Developer]
+  $description[Founded a Bug ir error?\nContact My Developer to Report!\n[Discord](https://discord.com/users/1207510451005427783)]
+  $footer[Made by Shadow]
+  $color[Blue]
+  
